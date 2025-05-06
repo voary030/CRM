844fee234dec4f00fd35ddbe2b54101bff794c33
Voary <voary030@gmail.com>
2025-05-06 23:19:38 +0300

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -71,7 +71,7 @@ return [
 	'database' => [
 		// uncomment the below 4 lines for mysql
 		 'host' => 'localhost',
-		 'dbname' => 'budgetbarbershop',
+		 'dbname' => 'budget',
 		 'user' => 'root',
 		 'password' => '',
 
