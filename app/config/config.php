52495ca95c6d658826a1b8b2264d896cab265447
tsilakelyy <voary030@gmail.com>
2025-05-06 22:39:17 +0300

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -32,7 +32,7 @@ $app->path(__DIR__ . $ds . '..' . $ds . '..');
 $app->path(__DIR__ . $ds . '..' . $ds . 'controllers');
 
 // This is where you can set some flight config variables. 
-$app->set('flight.base_url', '/projet_SI/Budget_SI'); // if this is in a subdirectory, you'll need to change this
+$app->set('flight.base_url', '/S4/CRM'); // if this is in a subdirectory, you'll need to change this
 $app->set('flight.case_sensitive', false); // if you want case sensitive routes, set this to true
 $app->set('flight.log_errors', true); // if you want to log errors, set this to true
 $app->set('flight.handle_errors', false); // if you want flight to handle errors, set this to true, otherwise Tracy will handle them
